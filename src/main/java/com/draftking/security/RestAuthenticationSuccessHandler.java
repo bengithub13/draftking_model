59926a89bcc8c9006863dca7505c0856dd9fc522
BenPoon <ben.poon@gmail.com>
2015-10-04 14:42:38 -0400

    security added to feature

--- src/main/java/com/draftking/security/RestAuthenticationSuccessHandler.java
+++ src/main/java/com/draftking/security/RestAuthenticationSuccessHandler.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
  
-@Component
+@Component("RestAuthenticationSuccessHandler")
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
  
     @Override
